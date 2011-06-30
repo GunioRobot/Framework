0f75b3b3f6f74d7cae21dec9cfdeb544732c3954
moritz <moritz@dead-pixel.de>
2011-06-30 21:26:29 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
+//Define console if not defined
+if( typeof console == 'undefined' ) {
+      var console = {
+            assert            : function() {}
+            ,clear            : function() {}
+            ,count            : function() {}
+            ,debug            : function() {}
+            ,dir              : function() {}
+            ,dirxml           : function() {}
+            ,error            : function() {}
+            ,exception        : function() {}
+            ,group            : function() {}
+            ,groupCollapsed   : function() {}
+            ,groupEnd         : function() {}
+            ,info             : function() {}
+            ,log              : function() {}
+            ,notify           : function() {}
+            ,profile          : function() {}
+            ,profileEnd       : function() {}
+            ,table            : function() {}
+            ,time             : function() {}
+            ,timeEnd          : function() {}
+            ,trace            : function() {}
+            ,warn             : function() {}
+      };
+}
+
 // jQuery Document Ready
-
 $(document).ready(function() {
-
     //Snippet to load further scripts, if necessary 
     if ($('selector').length) {
         $.getScript('scriptUrl.js', function() {
             init();
         });
     };
-
-}); 
+});
